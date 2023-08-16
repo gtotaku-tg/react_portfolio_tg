718f5be3506658212e81861d1260dedc11e63a35
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-08-16 01:28:24 -0400

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const handleClick = () => setNav(!nav)
         </div>
 
         {/* mobile menu */}
-        <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+        <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[#8E9AAF] flex flex-col justify-center items-center'}>
             <li className='py-6 text-4xl'><Link onClick={handleClick} to ='home' smooth={true} duration={500}> 
                     Home 
                 </Link></li>
